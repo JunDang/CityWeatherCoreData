82b916553146a1c886ee753d9823737fbb7be136
Jun Dang <jun.dang8@gmail.com>
2019-06-02 22:09:26 -0400

    Finished Project

--- CityWeather-3/AppDelegate.swift
+++ CityWeather-3/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSPlacesClient.provideAPIKey("AIzaSyBXsG03WdMqm1k_PAA4cVTyqur2p7Qfq78")
+        GMSPlacesClient.provideAPIKey("input the api key")
         return true
     }
 
